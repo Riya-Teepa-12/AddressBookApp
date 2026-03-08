e109f13033b8db74ccc3dd099b7a6d36dd07a5cc
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-03-08 09:13:27 +0530

    [Riya]: UC14 – Implemented CSV file support using OpenCSV to read and write contacts with unit tests.

--- src/main/java/com/addressbookapp/service/AddressBookService.java
+++ src/main/java/com/addressbookapp/service/AddressBookService.java
@@ -2,6 +2,7 @@ package com.addressbookapp.service;
 
 import com.addressbookapp.model.AddressBook;
 import com.addressbookapp.model.Contact;
+import com.addressbookapp.util.CSVUtil;
 import com.addressbookapp.util.FileUtil;
 
 import org.springframework.stereotype.Service;
@@ -236,10 +237,26 @@ public class AddressBookService {
 
         FileUtil.writeContactsToFile(filePath, book.getContacts());
     }
-
-
+    
     public List<Contact> loadContactsFromFile(String filePath) {
 
         return FileUtil.readContactsFromFile(filePath);
+    }
+    
+    public void saveContactsToCSV(String bookName, String filePath) {
+
+        AddressBook book = addressBooks.get(bookName);
+
+        if(book == null) {
+            return;
+        }
+
+        CSVUtil.writeContactsToCSV(filePath, book.getContacts());
+    }
+
+
+    public List<Contact> loadContactsFromCSV(String filePath) {
+
+        return CSVUtil.readContactsFromCSV(filePath);
     }
 }
